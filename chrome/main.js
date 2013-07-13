cd4694b96bea72e228b086fdf5b4cbf356d70a47
anotheranshu <anotheranshu@gmail.com>
2013-07-13 02:18:44 -0700

    Added code to interact with selection text.  May break with Lucy's update.

--- chrome/main.js
+++ chrome/main.js
@@ -1,1 +1,11 @@
 chrome.contextMenus.create({"title": "Text", "contexts": ["selection"]});
+
+function onClickHandler(info, tab) {
+  alert(info.selectionText);
+};
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+/*chrome.contextMenus.onClicked.addListener(function(OnClickData info, tabs.Tab tab) {
+  alert(info.selectionText());
+}); */

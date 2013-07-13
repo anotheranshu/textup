95b4985eaa3586f2f1b4b3becb86697e632b53c8
Lucy Guo <lguo@andrew.cmu.edu>
2013-07-13 02:12:10 -0700

    Created plugin that lets you edit text on a page

---
+++ chrome/page-text editor/bookmarklet.js
@@ -0,0 +1,1 @@
+(function () {if (document.documentElement.contentEditable === false || document.designMode === "off") {document.body.contentEditable='true';document.designMode='on';void 0;} else if (document.documentElement.contentEditable === true || document.designMode === "on") {document.body.contentEditable='false';document.designMode='off';void 0;}})();

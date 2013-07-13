c0b4c233f7d0af44f6a2f179d28b89bc89a9b804
anotheranshu <anotheranshu@gmail.com>
2013-07-13 07:15:13 -0700

    Huge commit, added js-only messages to GroupMe.  Currently only sending to Anshu.

--- chrome/main.js
+++ chrome/main.js
@@ -1,19 +1,25 @@
 chrome.contextMenus.create({"title": "TextUp: Send", "contexts": ["selection"]});
 
 function onClickHandler(info, tab) {
-  alert(info.selectionText);
 
-   $.ajax({
-      type: "GET",
-      url: "http://twitterautomate.com/testapp/sendnotifications.php",
-      data: {'variable': info.selectionText}
-    }).done(function( data) {
-      alert( "Request is done" );
-    });
+
+  var big_string = info.selectionText;
+
+  var i = 0;
+  var splits = [];
+  while(i < big_string.length)
+  {
+      splits.push(big_string.substr(i, i + 150));
+      i += 150;
+  }
+
+  alert(splits.length);
+
+  for (var j = 0; j < splits.length; j++) {
+    $.post("https://api.groupme.com/v3/bots/post", '{"bot_id": "7d2d271d677a378a39b232aab9", "text":"' + splits[j] + '"}');
+  }
+  alert(big_string); 
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
-/*chrome.contextMenus.onClicked.addListener(function(OnClickData info, tabs.Tab tab) {
-  alert(info.selectionText());
-}); */
